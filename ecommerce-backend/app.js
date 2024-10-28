a9cb3c59fed1782ddb269d23fc0b4b8e3e85296c
Yaswanth-Perumalla <yaswanth.perumalla@go-yubi.com>
2024-10-28 10:27:03 +0530

    Pushed build changes

--- ecommerce-backend/app.js
+++ ecommerce-backend/app.js
@@ -18,8 +18,8 @@ app.use(express.urlencoded({ extended: false }));
 
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+//    useNewUrlParser: true,
+//    useUnifiedTopology: true
 }).then(() => {
     console.log("MongoDB connected");
 }).catch((error) => {
